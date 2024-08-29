d218248cc8cb96bf697c1c36d959806bc1320fba
taeyeon <taenytaeni@gmail.com>
2024-08-29 13:25:33 +0900

    Commit Test

--- src/main/java/com/example/hotel/controller/MainController.java
+++ src/main/java/com/example/hotel/controller/MainController.java
@@ -2,9 +2,13 @@ package com.example.hotel.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
 public class MainController {
-
+    @GetMapping
+    public String index() {
+        return "index";
+    }
 }
