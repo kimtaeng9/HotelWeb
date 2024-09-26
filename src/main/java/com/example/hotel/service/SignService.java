dde432e24b794711de75cb7fd599f0caf75229b4
kimtaeng9 <89076116+kimtaeng9@users.noreply.github.com>
2024-09-26 09:23:36 +0900

    # test

--- src/main/java/com/example/hotel/service/SignService.java
+++ src/main/java/com/example/hotel/service/SignService.java
@@ -8,6 +8,5 @@ public interface SignService {
     User findUserByUsername(String username);
     User signUp(UserForm userForm);
     User signIn(String email, String password);
-
     User findUserById(Long userId);
 }
